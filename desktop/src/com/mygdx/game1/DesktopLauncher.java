a9def742ae66066bf3704cfc12b186e3301bca2e
CPulido <111784057+Jah335@users.noreply.github.com>
2022-11-04 10:22:44 -0500

    Get rid of window title

--- desktop/src/com/mygdx/game1/DesktopLauncher.java
+++ desktop/src/com/mygdx/game1/DesktopLauncher.java
@@ -12,7 +12,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Game1");
+		config.setTitle("");
 		config.setWindowedMode(600, 650);
 		config.setResizable(false);
 		new Lwjgl3Application(new MyGdxGame(), config);
